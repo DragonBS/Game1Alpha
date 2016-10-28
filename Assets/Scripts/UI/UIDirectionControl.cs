cea3286da20230e1321a4a9f081b74a6d8fb21e6
DragonBS <milkotodorov@hotmail.com>
2016-10-28 20:35:43 +0300

    Update assets with correct base

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -1,16 +1,23 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UIDirectionControl : MonoBehaviour
 {
-    public bool m_UseRelativeRotation = true;  
+    // This class is used to make sure world space UI
+    // elements such as the health bar face the correct direction.
 
 
-    private Quaternion m_RelativeRotation;     
+    public bool m_UseRelativePosition = true;     // Use relative position should be used for this gameobject?
+    public bool m_UseRelativeRotation = true;     // Use relative rotation should be used for this gameobject?
+
+
+    private Vector3 m_RelativePosition;         // The local position at the start of the scene.
+    private Quaternion m_RelativeRotation;      // The local rotatation at the start of the scene.
 
 
     private void Start()
     {
-        m_RelativeRotation = transform.parent.localRotation;
+        m_RelativePosition = transform.localPosition;
+        m_RelativeRotation = transform.localRotation;
     }
 
 
@@ -18,5 +25,8 @@ public class UIDirectionControl : MonoBehaviour
     {
         if (m_UseRelativeRotation)
             transform.rotation = m_RelativeRotation;
+
+        if (m_UseRelativePosition)
+            transform.position = transform.parent.position + m_RelativePosition;
     }
 }
