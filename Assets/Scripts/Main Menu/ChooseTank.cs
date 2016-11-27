7c78bd4aa61585e3bf7dbd3e56c54538465c54dd
gofuckyourself <go@fuck.thyne.self>
2016-11-27 15:50:33 +0200

    Fuck this shit - I'm out. One thing left to do - character select on hotplate menu, but I ain't fucking doing it - I'm a month ahead of schedule and am taking unpaid time off.

---
+++ Assets/Scripts/Main Menu/ChooseTank.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChooseTank : MonoBehaviour
+{
+    public Button tankMenuOpenButton;
+    public Image selectedTankImage;
+
+    public void SpriteChange()
+    {
+        tankMenuOpenButton.image.sprite = selectedTankImage.sprite;
+    }
+}
